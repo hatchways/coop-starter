5a8759a462fb82fe9d700dcbca5e3e660655b084
Ugur Akin <ugurakinsoftware@gmail.com>
2021-12-03 17:16:29 +0300

    Added a hardcoded random card display.

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -3,8 +3,21 @@ import useStyles from './useStyles';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
 import { useHistory } from 'react-router-dom';
-import { AppBar, Toolbar, CircularProgress, IconButton, Typography, Menu, MenuItem } from '@mui/material';
+import {
+  AppBar,
+  Toolbar,
+  CircularProgress,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  CardContent,
+  Card,
+  Grid,
+  CardMedia,
+} from '@mui/material';
 import { Menu as MenuIcon, AccountCircle } from '@mui/icons-material';
+import { Box } from '@mui/system';
 
 export default function Dashboard(): JSX.Element {
   const classes = useStyles();
@@ -35,47 +48,70 @@ export default function Dashboard(): JSX.Element {
   };
 
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          My App
-        </Typography>
-        {loggedInUser && (
-          <div>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleMenu}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorEl}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Profile</MenuItem>
-              <MenuItem onClick={handleClose}>My account</MenuItem>
-            </Menu>
-          </div>
-        )}
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            My App
+          </Typography>
+          {loggedInUser && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Card sx={{ flex: '1 0 auto' }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image="https://picsum.photos/250/150"
+              alt="A randomly generated image"
+            />
+            <CardContent>
+              <Typography variant="h6" component="h6">
+                Did you know that,
+              </Typography>
+              <Typography variant="body1">
+                The world’s oldest surviving piano is at the Metropolitan Museum of Art in New York City. The instrument
+                dates back to 1720, and was made by an Italian man named Bartolomeo Cristofori?
+              </Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
+    </>
   );
 }
